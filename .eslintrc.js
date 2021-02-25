8c7d7e40ed283d69975f1ea889d0cde894be86bd
Maksim Sinik <maksim@sinik.it>
2021-02-25 17:02:24 +0100

    updates deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
