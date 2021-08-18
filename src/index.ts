aa47ec5dbb9c065372d63f95141c85ade13e0652
fox1t <maksim@sinik.it>
2021-08-18 14:13:56 +0200

    Simplifies the project scripts
    updates deps

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Using Promises without this module might cause file descriptor and memory leaks: https://github.com/mcollina/make-promises-safe
+// Using Promises without this module might cause memory leaks: https://github.com/mcollina/make-promises-safe
 import 'make-promises-safe'
 
 export default (): string => 'put your code here'
